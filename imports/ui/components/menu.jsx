f707cf4d60fb62d197d22f0b338514aa5a891b00
Shaika-Dzari <shaikadzari@4nakama.net>
2016-09-08 20:44:31 -0400

    Add sass

--- imports/ui/components/menu.jsx
+++ imports/ui/components/menu.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { IndexLink, Link } from 'react-router';
 
 export const Menu = () => (
-  <ul>
+  <ul className="menu">
     <li><IndexLink to="/" activeClassName="active">Index</IndexLink></li>
     <li><Link to="/one" activeClassName="active">Page One</Link></li>
     <li><Link to="/two" activeClassName="active">Page Two</Link></li>
